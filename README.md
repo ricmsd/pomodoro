bc07e8fdc6fcea1354ca61f1282e0524bf4b5204
ricmsd <ricmsd@gmail.com>
2023-09-22 21:48:59 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,33 @@
+# Pomodoro
+Pomodoro is a pomodoro timer and a desktop application.
+
+## Screenshot
+![Pomodoro](https://raw.githubusercontent.com/ricmsd/pomodoro/main/docs/screenshot.png)
+
+## How to use
+Operation is simple.
+- The timer starts when you start the application.
+- The timer stops after 30 minutes.
+- Clicking on the screen restarts the timer from zero.
+
+## How to build
+### requirements
+- Node.js v18.17.1
+
+### Trial run
+Clone the repository and run the npm command.
+
+    git clone https://github.com/ricmsd/pomodoro.git
+    cd pomodoro
+    npm install
+    npm run start
+
+### Generate installer
+Running `npm run make` instead of `npm run start` will generate an installer (.exe).
+
+    git clone https://github.com/ricmsd/pomodoro.git
+    cd pomodoro
+    npm install
+    npm run make
+
+The installer is located in the `podomoro` folder `out/make/... /pomodoro-X.X.X Setup.exe` (where `X.X.X` is the version).
---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const Store = require('electron-store');
+const store = new Store();
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: store.get('browserWindow.width') || 800,
+        height: store.get('browserWindow.height') || 600,
+        alwaysOnTop: true,
+        // opacity: 0.75
+    })
+    win.on('resize', function() {
+        const size = win.getSize();
+        store.set('browserWindow.width', size[0]);
+        store.set('browserWindow.height', size[1]);
+    });
+
+    win.setMenu(null);
+    win.loadFile('pomodoro.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
